282a256059ba3ba58c124929b44093fc255eae75
webmvw <masudrana.bbpi@gmail.com>
2020-07-22 16:44:17 +0600

    portfolio version-1

---
+++ js/scritpt.js
@@ -0,0 +1,44 @@
+
+(function($){
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 200) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+  
+
+  $(document).ready(function() { 
+
+    // portfolio mixitup  
+    $('.portfolio_item').mixItUp({
+      animation: {
+        effects: 'rotateY'
+      }
+    }); 
+
+    // portfolio prettyphoto
+    $("a[rel^='prettyPhoto']").prettyPhoto();
+
+
+    $('.scrollToTop').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
+
+    // jquery counter plugin
+    $('.single_counter h5').counterUp({
+      delay: 10, // the delay time in ms
+      time: 2000 // the speed time in ms
+    }); 
+
+    // smoothscroll plugin
+    var scroll = new SmoothScroll('a[href*="#"]');
+
+    
+    $('.main_menu').slicknav();
+
+  });
+})(jQuery);
